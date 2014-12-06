d29724904d522da0731da330f57d28afbf3085ee
Anu <anu@anu-G31T-M7.(none)>
2014-12-06 23:55:59 +0530

    two way association

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,4 +1,13 @@
 class Branch < ActiveRecord::Base
 	scope :in_location, lambda { |location| where("city LIKE ? OR zip LIKE ? ", location["city"], location["zip"] ) }
 	belongs_to :branchable, :polymorphic => true
+	
+	has_many :branch_connects
+	has_many :advertisements, :through => :branch_connects, :source => :connectable, :source_type => "Advertisement"
+	has_many :deals, :through => :branch_connects, :source => :connectable, :source_type => "Deal"
+	has_many :sales, :through => :branch_connects, :source => :connectable, :source_type => "Sale"
+	has_many :flyers, :through => :branch_connects, :source => :connectable, :source_type => "Flyer"
+	has_many :video_advs, :through => :branch_connects, :source => :connectable, :source_type => "VideoAdv"
+	has_many :coupens, :through => :branch_connects, :source => :connectable, :source_type => "Coupen"
+
 end
