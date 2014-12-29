067168166b0474f8904cd21c18d1fc45b5d28af1
Anu <anu@anu-G31T-M7.(none)>
2014-12-30 01:02:08 +0530

    pg fix on migration

--- db/migrate/20141228184143_create_templates.rb
+++ db/migrate/20141228184143_create_templates.rb
@@ -5,7 +5,7 @@ class CreateTemplates < ActiveRecord::Migration
       t.string :logo_position, default: "center"
       t.string :logo_image_type
       t.string :bg_color, default: "#FFF"
-      t.boolean :bg_image, default: 0
+      t.boolean :bg_image, default: false
       t.integer :no_of_images, default: 0
 
       t.timestamps
